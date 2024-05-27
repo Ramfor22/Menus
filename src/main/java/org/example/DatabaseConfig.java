90e7dc5ec766b499cd35c4c8ac9cc02595b2cac4
Harold Muschang <haroldmuschang@Harolds-MacBook-Pro.local>
2024-05-27 16:41:17 +0200

    Ajout du sous-menu pour afficher les commandes

--- src/main/java/org/example/DatabaseConfig.java
+++ src/main/java/org/example/DatabaseConfig.java
@@ -1,30 +1,48 @@
 package org.example;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class DatabaseConfig {
-    public static String user;
-    public static String password;
-    public static int port;
-    public static String dbName;
+    private static String user;
+    private static String password;
+    private static String port;
+    private static String name;
 
-    public static void loadConfig(String filePath) {
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(filePath);
+    public static void loadFromXML() throws Exception {
+        File file = new File("src/main/resources/db_config.xml"); // Chemin relatif au fichier XML
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(file);
 
-            Element root = document.getDocumentElement();
+        doc.getDocumentElement().normalize();
 
-            user = root.getElementsByTagName("user").item(0).getTextContent();
-            password = root.getElementsByTagName("password").item(0).getTextContent();
-            port = Integer.parseInt(root.getElementsByTagName("port").item(0).getTextContent());
-            dbName = root.getElementsByTagName("name").item(0).getTextContent();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Element databaseElement = (Element) doc.getElementsByTagName("database").item(0);
+
+        user = databaseElement.getElementsByTagName("user").item(0).getTextContent();
+        password = databaseElement.getElementsByTagName("password").item(0).getTextContent();
+        port = databaseElement.getElementsByTagName("port").item(0).getTextContent();
+        name = databaseElement.getElementsByTagName("name").item(0).getTextContent();
+    }
+
+    public static String getUser() {
+        return user;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static String getPort() {
+        return port;
+    }
+
+    public static String getName() {
+        return name;
     }
 }
+
--- src/main/java/org/example/DatabaseManager.java
+++ src/main/java/org/example/DatabaseManager.java
@@ -9,28 +9,27 @@ public class DatabaseManager {
     private Connection connection;
 
     public DatabaseManager() {
-        DatabaseConfig.loadConfig("path/to/database_config.xml");
-        String url = "jdbc:mysql://localhost:" + DatabaseConfig.port + "/" + DatabaseConfig.dbName;
         try {
-            connection = DriverManager.getConnection(url, DatabaseConfig.user, DatabaseConfig.password);
-        } catch (SQLException e) {
+            DatabaseConfig.loadFromXML(); // Appel sans paramètres
+            String url = "jdbc:mysql://localhost:" + DatabaseConfig.getPort() + "/" + DatabaseConfig.getName();
+            String user = DatabaseConfig.getUser();
+            String password = DatabaseConfig.getPassword();
+
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public Connection getConnection() {
-        return connection;
-    }
-
-    // Méthode pour ajouter une commande dans la base de données
-    public void addCommande(Commande commande) {
+    public void insertCommande(String table, String plat, String accompagnement, String dessert, double prixTotal) {
         String query = "INSERT INTO commandes (table_num, plat, accompagnement, dessert, prix_total) VALUES (?, ?, ?, ?, ?)";
+
         try (PreparedStatement stmt = connection.prepareStatement(query)) {
-            stmt.setString(1, commande.getTableNum());
-            stmt.setString(2, commande.getPlat());
-            stmt.setString(3, commande.getAccompagnement());
-            stmt.setString(4, commande.getDessert());
-            stmt.setDouble(5, commande.getPrixTotal());
+            stmt.setString(1, table);
+            stmt.setString(2, plat);
+            stmt.setString(3, accompagnement);
+            stmt.setString(4, dessert);
+            stmt.setDouble(5, prixTotal);
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
